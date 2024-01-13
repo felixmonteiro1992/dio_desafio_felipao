abf979f032c9bb9925efcdf7316b9ea1dc349bba
Felix  Monteiro <124623336+felixmonteiro1992@users.noreply.github.com>
2024-01-13 19:17:58 -0300

    Add files via upload

---
+++ DesafioClassificadorDenNivelDeHeroi.js
@@ -0,0 +1,46 @@
+// Variáveis nome e xp do herói
+let nomeHeroi = "Dio.me"
+let XP = 1000
+
+let nivel
+
+
+if (XP <= 1000) {
+    nivel = "Ferro";
+}
+else if(XP >= 1001 && XP <= 2000){
+    nivel = "Broze"
+}
+else if (XP >= 2001 && XP <= 5000){
+    nivel="Prata"
+}
+else if (XP >= 5001 && XP <= 7000){
+    nivel="Ouro"
+}
+else if (XP >= 7001 && XP <= 8000 ){
+    nivel = "Platina"
+}
+else if (XP >= 8001 && XP <= 9000){
+    nivel = "Ascendente"
+}
+else if (XP >= 9001 && XP <= 10000){
+    nivel = "Imortal"
+}
+else{
+    nivel = "Radiante"
+}
+
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
